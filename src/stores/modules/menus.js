2eda7cf33efe0ef04c97cbf1f687dba23d3fc24a
phuptdot <phupt@dol-tech.com>
2020-06-04 20:06:40 +0700

    clear cache

--- src/stores/modules/menus.js
+++ src/stores/modules/menus.js
@@ -20,17 +20,29 @@ export default {
     },
     actions: {
         async loadMenus({commit}) {
-
+            if (localStorage.getItem('menus')) {
+                commit('setMenus', JSON.parse(localStorage.getItem('menus')));
+                return
+            }
             let response = await axios.get(API.menu.list);
-            console.log(API.menu.list);
             if (response.data)
+            {
+                localStorage.setItem('menus', JSON.stringify(response.data));
                 commit('setMenus', response.data)
+            }
         },
 
         async getMenuDetail({  commit  }, id) {
+            let key = 'menus_'+ id;
+            if (localStorage.getItem(key)) {
+                commit('setMenuItem', JSON.parse(localStorage.getItem(key)));
+                return
+            }
+
             console.log(API.menu.detail + id);
             let response = await axios.get(API.menu.detail  + id);
             if (response.data) {
+                localStorage.setItem(key, JSON.stringify(response.data));
                 commit('setMenuItem', response.data)
             }
         }
